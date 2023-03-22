e02b3067fc40330330f07b8544cf9f6bf2d176c4
Breno <breno.ar.andrade@gmail.com>
2023-03-22 11:36:20 -0300

    atualizadas as funcoes new e list

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # todo-list-js
+
+Just an Todo App using only prompt and alert to give feedback to the user.
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let action = "";
 
 const todos = [];
 
-while (action !== 'quit') {
+while (action !== 'quit' && action !== 'q') {
 
     action = prompt(`What would you like to do?
     - new: create new todo
@@ -14,22 +14,24 @@ while (action !== 'quit') {
     if (action === "new") {
         const newTodo = prompt("Enter new todo");
         todos.push(newTodo);
-        alert('New task added!');
+        alert(`You added '${newTodo}' to the list!`);
     } else if (action === "list") {
+        let listaCompleta = "";
         for (todo of todos) {
-            console.log(`${todos.indexOf(todo)}: ${todo}`);
+            let str = `${todos.indexOf(todo)}: ${todo} \n`;
+            listaCompleta = listaCompleta.concat(str);
         }
-        console.log('-------------------------------');
+        alert(`Your Todos\n${listaCompleta}`);
     } else if (action === 'delete') {
-        let indice = prompt('Qual o indice do todo?');
+        let indice = prompt("What's the index?");
         while (todos[indice] === undefined) {
-            console.log('This index does not exist');
-            indice = prompt('Qual o indice do todo?');
+            alert('This index does not exist');
+            indice = prompt("What's the index?");
         }
         todos.splice(parseInt(indice), 1);
     }  
     else {
-        if (action !== 'quit') {console.log("Please, enter a valid command!")};
+        if (action !== 'quit' && action !== 'q') {alert("Please, enter a valid command!")};
     }
 }
 console.log("Leaving the app!");
