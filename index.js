446b287cb005b107accc21a95852281a841069b4
Breno <breno.ar.andrade@gmail.com>
2023-03-23 16:09:52 -0300

    adicionando mais alguns feedbacks para o usuario

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const todos = [];
 while (action !== 'quit' && action !== 'q') {
 
     action = prompt(`What would you like to do?
-    - new: create new todo
-    - list: list todos
-    - delete: delete todo based on the index
+    - new: create new to do
+    - list: list to dos
+    - delete: delete to do based on the index
     - quit: leave the app
     `);
     
     if (action === "new") {
-        const newTodo = prompt("Enter new todo");
+        const newTodo = prompt("Enter new to do");
         todos.push(newTodo);
         alert(`You added '${newTodo}' to the list!`);
     } else if (action === "list") {
@@ -21,17 +21,27 @@ while (action !== 'quit' && action !== 'q') {
             let str = `${todos.indexOf(todo)}: ${todo} \n`;
             listaCompleta = listaCompleta.concat(str);
         }
-        alert(`Your Todos\n${listaCompleta}`);
+        alert(`Your To dos\n${listaCompleta}`);
     } else if (action === 'delete') {
-        let indice = prompt("What's the index?");
+        let indice;
+
         while (todos[indice] === undefined) {
-            alert('This index does not exist');
-            indice = prompt("What's the index?");
+            
+            let listaCompleta = "";
+            for (todo of todos) {
+                let str = `${todos.indexOf(todo)}: ${todo} \n`;
+                listaCompleta = listaCompleta.concat(str);
+            }   
+            // alert(`Your To dos\n${listaCompleta}`);
+            
+            indice = prompt(`What's the index?\n${listaCompleta}`);
+            if (todos[indice] === undefined) {alert('This index does not exist')};
         }
+        alert(`You have deleted ${todos[indice]} from your to do list!` );
         todos.splice(parseInt(indice), 1);
     }  
     else {
         if (action !== 'quit' && action !== 'q') {alert("Please, enter a valid command!")};
     }
 }
-console.log("Leaving the app!");
+alert("Leaving the app!");
